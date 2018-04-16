94a6071923402e08764d742b9c4ab8582aa050ef
Paulo Jesus <paulo.jesus@lottoland.com>
2018-04-16 18:05:56 +0200

    bootstraping of the spring applications

---
+++ player-service/src/main/java/io/devchaos/poc/playerservice/PlayerServiceApplication.java
@@ -0,0 +1,12 @@
+package io.devchaos.poc.playerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerServiceApplication.class, args);
+	}
+}
